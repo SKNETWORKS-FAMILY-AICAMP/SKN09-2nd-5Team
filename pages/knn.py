478a523f52c73af6afd1029fdbd0279837620a49
Leegwangwoon <leegwangwoon@naver.com>
2025-02-10 16:49:44 +0900

    add_streamlit_and_add_images

---
+++ pages/knn.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("k-최근접 이웃 알고리즘")
+
+st.image("./image/knn_receiver curve.png", caption="이미지 1",use_container_width=True)
+st.image("./image/knn calibration curve.png", caption="이미지 2", use_container_width=True)
+st.image("./image/knn_confusion_matrix.png", caption="이미지 3", use_container_width=True)
---
+++ pages/lightgbm.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("lightGBM")
+
+st.image("./image/lightGBM_ROC.png", caption="이미지 1",use_container_width=True)
+st.image("./image/light_gbm_Calibration.png", caption="이미지 2", use_container_width=True)
+st.image("./image/lightgbm_confusion.png", caption="이미지 3", use_container_width=True)
