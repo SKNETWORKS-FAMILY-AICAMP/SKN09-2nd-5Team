da02d616e2f0e9c6440ac90051713e2695dd7855
Leegwangwoon <leegwangwoon@naver.com>
2025-02-10 17:21:27 +0900

    fix_typo

--- pages/RandomForest.py
+++ pages/RandomForest.py
@@ -4,13 +4,13 @@ st.title("RandomForest 분석")
 st.write("이 페이지에서는 RandomForest 모델 성능 분석을 보여줍니다.\n")
 
 st.subheader('Confusion Matrix')
-st.image("./image/RF_Confusion Matrix.png", caption="Confusion Matrix", use_column_width=True)
+st.image("./image/RF_Confusion Matrix.png", caption="Confusion Matrix", use_container_width=True)
 
 st.subheader('Calibration Curve')
-st.image("./image/RF_Calibration Curve.png", caption="Calibration Curve", use_column_width=True)
+st.image("./image/RF_Calibration Curve.png", caption="Calibration Curve", use_container_width=True)
 
 st.subheader('ROC Curve')
-st.image("./image/RF_ROC Curve.png", caption="ROC Curve", use_column_width=True)
+st.image("./image/RF_ROC Curve.png", caption="ROC Curve", use_container_width=True)
 
 # 설명 추가
 st.markdown('''
